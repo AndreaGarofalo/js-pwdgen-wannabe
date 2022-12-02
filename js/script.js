63d4bb5252e8477a546674a4ad00786b4d0bd7c5
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-02 16:38:02 +0100

    added analisis

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("JS OK");
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+/* 
+1. Prendo l'elemento dal dom
+(1.b preparo una variabile con la string '21')
+2. chiedo all'utente il suo nome con prompt e lo salvo nella variabile firstName
+3. chiedo all'utente il suo cognome con prompt e lo salvo nella variabile lastName
+4. chiedo all'utente il suo colore preferito con prompt e lo salvo nella variabile favouriteColor
+5. creo la password 
+6. inserisco la password in html
+*/
